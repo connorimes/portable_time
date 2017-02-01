c9b4e12060de89892b2029a706275a2770fbf5f5
Connor Imes <connor.k.imes@gmail.com>
2017-02-01 14:18:23 -0600

    First stab at Windows impl

--- inc/ptime.h
+++ inc/ptime.h
@@ -8,6 +8,13 @@ extern "C" {
 #include <inttypes.h>
 #include <time.h>
 
+#if defined(_WIN32)
+struct timespec {
+  long int tv_sec;
+  long int tv_nsec;
+};
+#endif
+
 int ptime_clock_gettime(struct timespec* ts);
 
 uint64_t ptime_to_ns(struct timespec* ts);
--- src/ptime.c
+++ src/ptime.c
@@ -9,7 +9,67 @@
 #include <Windows.h>
 #endif
 
-// TODO: CLOCK_MONOTONIC
+#if defined(_WIN32)
+// See http://stackoverflow.com/questions/5404277/porting-clock-gettime-to-windows
+
+static LARGE_INTEGER getFILETIMEoffset() {
+  SYSTEMTIME s;
+  FILETIME f;
+  LARGE_INTEGER t;
+
+  s.wYear = 1970;
+  s.wMonth = 1;
+  s.wDay = 1;
+  s.wHour = 0;
+  s.wMinute = 0;
+  s.wSecond = 0;
+  s.wMilliseconds = 0;
+  SystemTimeToFileTime(&s, &f);
+  t.QuadPart = f.dwHighDateTime;
+  t.QuadPart <<= 32;
+  t.QuadPart |= f.dwLowDateTime;
+  return t;
+}
+
+static int clock_gettime_win32(int dummy, struct timespec *ts) {
+  (void) dummy;
+  LARGE_INTEGER           t;
+  FILETIME                f;
+  double                  microseconds;
+  static LARGE_INTEGER    offset;
+  static double           frequencyToMicroseconds;
+  static int              initialized = 0;
+  static BOOL             usePerformanceCounter = 0;
+
+  if (!initialized) {
+    LARGE_INTEGER performanceFrequency;
+    initialized = 1;
+    usePerformanceCounter = QueryPerformanceFrequency(&performanceFrequency);
+    if (usePerformanceCounter) {
+      QueryPerformanceCounter(&offset);
+      frequencyToMicroseconds = (double)performanceFrequency.QuadPart / 1000000.;
+    } else {
+      offset = getFILETIMEoffset();
+      frequencyToMicroseconds = 10.;
+    }
+  }
+  if (usePerformanceCounter) {
+    QueryPerformanceCounter(&t);
+  } else {
+    GetSystemTimeAsFileTime(&f);
+    t.QuadPart = f.dwHighDateTime;
+    t.QuadPart <<= 32;
+    t.QuadPart |= f.dwLowDateTime;
+  }
+
+  t.QuadPart -= offset.QuadPart;
+  microseconds = (double)t.QuadPart / frequencyToMicroseconds;
+  t.QuadPart = microseconds;
+  ts->tv_sec = t.QuadPart / 1000000;
+  ts->tv_usec = t.QuadPart % 1000000;
+  return 0;
+}
+#endif
 
 int ptime_clock_gettime(struct timespec* ts) {
 #if defined(__MACH__)
@@ -22,9 +82,7 @@ int ptime_clock_gettime(struct timespec* ts) {
   ts->tv_nsec = mts.tv_nsec;
   return 0;
 #elif defined(_WIN32)
-  // TODO
-  fprintf(stderr, "Unimplemented!\n")
-  exit(1);
+  return clock_gettime_win32(0, ts);
 #else
   return clock_gettime(CLOCK_REALTIME, ts);
 #endif
@@ -46,9 +104,9 @@ static const uint64_t ONE_BILLION_U64 = 1000000000;
   mach_port_deallocate(mach_task_self(), cclock);
   return mts.tv_sec * ONE_BILLION_U64 + mts.tv_nsec;
 #elif defined(_WIN32)
-  // TODO
-  fprintf(stderr, "Unimplemented!\n")
-  exit(1);
+  struct timespec ts;
+  clock_gettime_win32(0, &ts);
+  return ts.tv_sec * ONE_BILLION_U64 + ts.tv_nsec;
 #else
   struct timespec ts;
   // CLOCK_REALTIME is always supported, this should never fail
